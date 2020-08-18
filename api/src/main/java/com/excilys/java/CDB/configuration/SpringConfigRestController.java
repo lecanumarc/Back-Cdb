20faa3bff11e241b97d8a55c170413041b0a719a
lecanumarc <51373207+lecanumarc@users.noreply.github.com>
2020-08-18 12:27:26 +0200

    Custom query : filter by company and computer name

--- api/src/main/java/com/excilys/java/CDB/configuration/SpringConfigRestController.java
+++ api/src/main/java/com/excilys/java/CDB/configuration/SpringConfigRestController.java
@@ -60,15 +60,15 @@ public class SpringConfigRestController extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
-				// dont authenticate this particular request
-				.authorizeRequests()
-				.antMatchers("/authenticate", "/**").permitAll()
-				// all other requests need to be authenticated
-				.anyRequest().authenticated().and().
-				// make sure we use stateless session; session won't be used to
-				// store user's state.
-				exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		// dont authenticate this particular request
+		.authorizeRequests()
+		.antMatchers("/authenticate", "/**").permitAll()
+		// all other requests need to be authenticated
+		.anyRequest().authenticated().and().
+		// make sure we use stateless session; session won't be used to
+		// store user's state.
+		exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
 		// Add a filter to validate the tokens with every request
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
--- persistence/src/main/java/com/excilys/java/CDB/persistence/DAO/ComputerDAO.java
+++ persistence/src/main/java/com/excilys/java/CDB/persistence/DAO/ComputerDAO.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.excilys.java.CDB.model.Computer;
@@ -18,6 +20,9 @@ import com.excilys.java.CDB.model.Computer;
 public interface ComputerDAO extends JpaRepository<Computer, Long> {
 	
 	Page<Computer> findByNameContaining(String string, Pageable page);
+	
+	@Query(value = "SELECT u FROM Computer u where u.name like %?1% or u.company.name like %?1%")
+	Page<Computer> filteredPage( String filter,  Pageable page);
 	List<Computer> findAllByCompanyId(long id);
 	int countByNameContaining(String search);
 }
